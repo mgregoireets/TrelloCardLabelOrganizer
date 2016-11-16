894eec310af4af4d70a0f25446f6e29371ca4b90
Martin Gregoire <Martin.gregoire.2@ens.etsmtl.ca>
2016-11-15 22:56:39 -0500

    Comparator to compare labels

--- Tests/TestLabelReader.py
+++ Tests/TestLabelReader.py
@@ -1,23 +1,45 @@
 import unittest
+from TrelloLabelReader import comparator
+from TrelloLabelReader import parse_args
 
 
-class MyTestCase(unittest.TestCase):
+class AppArgs(unittest.TestCase):
     def test_args_appkey_not_provided(self):
-        from TrelloLabelReader import parse_args
         self.assertTrue(not parse_args(0))
 
     def test_args_appkey_valid(self):
-        from TrelloLabelReader import parse_args
         self.assertTrue(parse_args("bar1bar1bar1bar1bar1bar1bar1bar1"))
 
     def test_args_appkey_invalid(self):
-        from TrelloLabelReader import parse_args
         self.assertFalse(parse_args("bar1"))
 
     def test_args_boardname_provided(self):
-        from TrelloLabelReader import parse_args
-        apikey="bar1bar1bar1bar1bar1bar1bar1bar1"
-        self.assertTrue(parse_args(apikey,"foobar"))
+        apikey = "bar1bar1bar1bar1bar1bar1bar1bar1"
+        self.assertTrue(parse_args(apikey, "foobar"))
+
+
+class TestComparator(unittest.TestCase):
+    def test_label_matched(self):
+        Bugcomparator="Bugs"
+        self.assertEquals(Bugcomparator, comparator("bug"))
+        self.assertEquals(Bugcomparator, comparator("bugs"))
+        self.assertEquals(Bugcomparator, comparator("bog"))
+        self.assertEquals(Bugcomparator, comparator("bogue"))
+        self.assertEquals(Bugcomparator, comparator("Bugs"))
+        self.assertEquals(Bugcomparator, comparator("Bug"))
+
+        ReleaseComparator="Release"
+        self.assertEquals(ReleaseComparator, comparator("release"))
+        self.assertEquals(ReleaseComparator, comparator("Release"))
+        self.assertEquals(ReleaseComparator, comparator("realese"))
+        self.assertEquals(ReleaseComparator, comparator("Realease"))
+
+        ProductLaunchComparator="Product launch"
+        self.assertEquals(ProductLaunchComparator, comparator("Product Launch"))
+        self.assertEquals(ProductLaunchComparator, comparator("Launch product"))
+        self.assertEquals(ProductLaunchComparator, comparator("Product launch"))
+        self.assertEquals(ProductLaunchComparator, comparator("Launch Product"))
+        self.assertEquals(ProductLaunchComparator, comparator("Product-Launch"))
 
 
 if __name__ == '__main__':
--- TrelloLabelReader.py
+++ TrelloLabelReader.py
@@ -1,7 +1,11 @@
 import sys
+import difflib
 apikey=0
 boardName=0
+dictionary={"Bugs","Release","Product launch"}
 
+#TrelloLabelReader.py appkey boardName
+#BoardName is optional
 def get_args():
     if len(sys.argb)<=1 or len(sys.argv)>3:
         return False
@@ -11,7 +15,7 @@ def get_args():
         boardName=sys.argv[2]
         return True
 
-
+#Validates if the given application key is valid
 def parse_args(appkey,board=None):
     if appkey and type(appkey)==str and len(appkey)==32:
         if board and type(board)!=str:
@@ -19,3 +23,7 @@ def parse_args(appkey,board=None):
         return True
     else:
         return False
+#Compares the label with a set of predetermined, correctly orthographied Labels
+#ignores labels that have a %match < 0.22
+def comparator(label,cutoff=0.22):
+    return difflib.get_close_matches(label,dictionary,cutoff=cutoff)[0]
