df66ff64ccfb4c62d2b42f73493ac5c69864ca4e
Martin Gregoire <Martin.gregoire.2@ens.etsmtl.ca>
2016-11-16 19:24:58 -0500

    Show suggestion on the label a card should have

--- TrelloLabelReader.py
+++ TrelloLabelReader.py
@@ -12,7 +12,7 @@ cards = list()
 
 dictionary = {"Bugs", "Release", "Product launch"}
 
-fixedCardLabels = {}
+fixedCardLabels = dict()
 
 
 # TrelloLabelReader.py appkey boardName
@@ -41,7 +41,7 @@ def parse_args(appkey, username, board=None):
 
 # Compares the label with a set of predetermined, correctly orthographied Labels
 # ignores labels that have a %match < 0.22
-def comparator(label, cutoff=0.22):
+def comparator(label, cutoff=0.2):
     return difflib.get_close_matches(label, dictionary, cutoff=cutoff)[0]
 
 
@@ -49,9 +49,14 @@ def comparator(label, cutoff=0.22):
 #  those in the dictionary
 def initialise_dictionary():
     for d in dictionary:
-        fixedCardLabels[d] = 0
+        fixedCardLabels[d] = list()
 
-#gets the card of the users specified board or of all his public boards
+#adds all cards in the board
+def get_board_cards(id):
+    cards.append(trello.boards.get_card(id))
+
+
+# gets the card of the users specified board or of all his public boards
 def getCards():
     boardIds = list()
 
@@ -59,13 +64,15 @@ def getCards():
     for board in boards:
         boardIds.append(board["id"])
     for id in boardIds:
-        cards.append(trello.boards.get_card(id))
+        get_board_cards(id)
 
-#gets all the public boards of the user
+
+# gets all the public boards of the user
 def get_member_boards():
     return trello.members.get_board(username)
 
-#gets the id of the users boards
+
+# gets the id of the users boards
 def get_board_id():
     global boardID
     boards = get_member_boards()
@@ -73,19 +80,34 @@ def get_board_id():
         if board["name"] == boardName:
             return board["id"]
 
-#show each label of a card
+
+# show each label of a card
 def show_card(name, labels):
-    print "Here are the labels for card: "+name
+    print "Here are the labels for card: " + name
     for l in labels:
         print l["name"]
 
-#extracts teh labels and the cards of the user
+
+# extracts teh labels and the cards of the user
 def parse_labels():
     for board in cards:
         for card in board:
             labels = card["labels"]
             cardname = card["name"]
             show_card(cardname, labels)
+
+#Shows the label each cards should have
+def fix_labels():
+    print "Here are how the labels should be fixed for each card: "
+    for board in cards:
+        for card in board:
+            cardname = card["name"]
+            for label in card["labels"]:
+                fixedCardLabels[comparator(label["name"])].append(cardname)
+        for fixedlabel in fixedCardLabels:
+            print fixedlabel + ": "
+            for fixedcard in fixedCardLabels[fixedlabel]:
+                print "    " + fixedcard
 
 
 initialise_dictionary()
@@ -100,3 +122,4 @@ if parse_args(apikey, username, boardName):
         getCards()
     if len(cards) > 0:
         parse_labels()
+        fix_labels()
